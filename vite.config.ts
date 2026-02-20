a515b061770610fc2883362e88d42ab5ed1dcb4c
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-02-20 19:30:14 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const databaseUrl = process.env.DATABASE_URL as string | undefined;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5173;
+
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 3000
+		port: port ? port : 5173,
 	}
 });
