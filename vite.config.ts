e7876bc1647022550d40c63b20398ed981205612
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-02-20 21:16:01 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
 	server: {
         host: true, 
 		port: 3000,
-       allowedHosts: [
+    allowedHosts: [
           'localhost',  // Always good practice
           '127.0.0.1'   // Also a good idea
-    ]
+          ]
 	}
 });
