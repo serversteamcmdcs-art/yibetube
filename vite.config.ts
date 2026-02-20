4dae2f2f543da0bbe212b081a4f15083c14d8d35
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-02-20 20:53:03 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	plugins: [react()],
+	
   server: {
+	  port : 3000,
     // 1. Allow the server to listen on all network interfaces (0.0.0.0)
     // This is necessary for Docker to expose the port correctly.
     host: true, 
     
     // 2. Explicitly allow the host name seen by the reverse proxy/browser.
     allowedHosts: [
-      BLOCKED_HOST, // The specific host from the error message
       'localhost',  // Always good practice
-      '127.0.0.1'   // Also a good idea
+      '127.0.0.1',   // Also a good idea
+	  'vibetube.c6t.ru',
+	  'vibetube.p7z.ru'
     ]
-	  port : 3000,
   }
 });
