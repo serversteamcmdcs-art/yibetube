a89e5159c8879c7daf6824901b5ee64e4514666e
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-02-20 22:28:06 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 5000,
-    strictPort: false,
     host: true,
-    allowedHosts: ['vibetube.p7z.ru', 'localhost', 'vibetube.c6t.ru']
+    allowedHosts: ['vibetube.p7z.ru']
+  },
+  build: {
+    outDir: 'dist' // папка со сборкой
   }
 });
