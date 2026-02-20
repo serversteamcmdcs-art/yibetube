90ef58c58593f1707c4bed0860af2d0f9d6c7cdf
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-02-20 21:32:01 +0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+const databaseUrl = process.env.DATABASE_URL as string | undefined;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5173;
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+    host: true,
+		port: 5000,
+	}
+});
